6031ab582c8f6d9ecc35003c087eb7a3280a3066
info-cppsp <info@cppsp.de>
2019-06-16 15:22:21 +0200

    Add validation WIP

--- src/main/java/hello/model/Listing.java
+++ src/main/java/hello/model/Listing.java
@@ -6,6 +6,11 @@ import java.util.UUID;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -21,18 +26,27 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Listing {
 
-	@Id
+	@Id @NotNull
 	private UUID id;
+	@NotNull
 	private String title;
+	@NotNull
 	private String description;
+	@NotNull
 	private UUID inventory_item_location_id;
+	@NotNull @Positive @Digits(integer = 0, fraction = 2)
 	private BigDecimal listing_price;
+	@NotNull @Size(min = 3, max = 3)
 	private String currency;
+	@NotNull @Positive
 	private int quantity;
+	@NotNull
 	private int listing_status;
+	@NotNull
 	private int marketplace;
 	@JsonFormat(pattern="MM/dd/yyyy")
 	private Date upload_time;
+	@NotNull @Email
 	private String owner_email_address;
 
 }
